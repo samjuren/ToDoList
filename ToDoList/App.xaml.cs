02e3e0b5f0b80af5cbd1698183ee58f2612cb8f2
Samuel Juren <sammjureen@outlook.com>
2024-05-20 17:57:34 -0300

    Add project files.

---
+++ ToDoList/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
