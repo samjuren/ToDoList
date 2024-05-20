05bd719f1be1f5c6ecab0b962cb0582df5e15d67
Samuel Juren <sammjureen@outlook.com>
2024-05-20 20:10:10 -0300

    Criado conexão com o SQLite

---
+++ ToDoList/DbContext/DataBaseHandler.cs
@@ -0,0 +1,49 @@
+﻿using SQLite;
+using ToDoList.Model;
+
+namespace ToDoList.DbContext
+{
+    public class DataBaseHandler
+    {
+        public static SQLiteConnection? Connection { get; set; }
+
+        public static void Init()
+        {
+            CreateConnection();
+            CreateTable();
+        }
+
+        private static void CreateConnection()
+        {
+            string databasePath = Path.Combine(FileSystem.AppDataDirectory, "ToDo_Entrevista.db");
+            Connection = new SQLiteConnection(new SQLiteConnectionString(databasePath));
+        }
+
+        private static void CreateTable()
+        {
+            Connection?.CreateTable(typeof(ToDoItem));
+        }
+
+        //Metedos de CRUP db
+
+        public static void Insert(object obj)
+        {
+            Connection?.Insert(obj);
+        }
+
+        public static void Delete(object obj)
+        {
+            Connection?.Delete(obj);
+        }
+
+        public static void Update(object obj)
+        {
+            Connection?.Update(obj);
+        }
+
+        public static List<T> GetAll<T>() where T : new()
+        {
+            return Connection.Table<T>().ToList();
+        }
+    }
+}
