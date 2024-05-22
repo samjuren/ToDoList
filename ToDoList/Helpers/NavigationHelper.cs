bf4b8cd8675164d4254f94aaf29ac2d5b31c0bd8
Samuel Juren <sammjureen@outlook.com>
2024-05-21 23:37:09 -0300

    Clean Code para reutilizar codigos

---
+++ ToDoList/Helpers/NavigationHelper.cs
@@ -0,0 +1,48 @@
+﻿namespace ToDoList.Helpers
+{
+    public class NavigationHelper
+    {
+        public static async Task<bool> DisplayMessage(string message)
+        {
+            return await Application
+                .Current?
+                .MainPage?
+                .DisplayAlert("Atençao", message, "Ok", "Cancelar");
+        }
+
+        public static async Task DisplayAlert(string message)
+        {
+            await Application
+                .Current?
+                .MainPage?
+                .DisplayAlert("Atençao", message, "Ok");
+        }
+
+        public static async Task PushAsync(Page page)
+        {
+            await Application
+                .Current?
+                .MainPage?
+                .Navigation?
+                .PushAsync(page);
+        }
+
+        public static async Task PopAsync()
+        {
+            await Application
+               .Current?
+               .MainPage?
+               .Navigation?
+               .PopAsync();
+        }
+
+        public static async Task PopModalAsync()
+        {
+            await Application
+               .Current?
+               .MainPage?
+               .Navigation?
+               .PopModalAsync();
+        }
+    }
+}
