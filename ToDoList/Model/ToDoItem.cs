c4fff91a06dc1588550ff82f05bd550b192a0c62
Samuel Juren <sammjureen@outlook.com>
2024-05-21 23:36:31 -0300

    ajuste na model

--- ToDoList/Model/ToDoItem.cs
+++ ToDoList/Model/ToDoItem.cs
@@ -1,16 +1,63 @@
 ﻿using SQLite;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ToDoList.Model
 {
     [Table("ToDoList")]
-    public class ToDoItem
+    public class ToDoItem : INotifyPropertyChanged
     {
         [PrimaryKey]
         [AutoIncrement]
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-        public int Status { get; set; }
 
+        private string? title;
+        public string? Title 
+        { 
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        private string? description;
+        public string? Description 
+        { 
+            get => description;
+            set
+            {
+                description = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        private bool isConcluded;
+        public bool IsConcluded
+        {
+            get => isConcluded;
+            set 
+            {
+                isConcluded = value;
+                OnPropertyChanged();
+            }
+        }
+        private string? status;
+        public string? Status 
+        { 
+            get => status;
+            set
+            {
+                status = value;
+                OnPropertyChanged();
+            } 
+        }
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
