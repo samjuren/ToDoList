60bbf414c12c2843242ca031e52389462cb3098f
Samuel Juren <sammjureen@outlook.com>
2024-05-21 23:36:24 -0300

    pasta de Converter

---
+++ ToDoList/Converter/BoolToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace ToDoList.Converter
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        //Bool
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return (bool)value ? Colors.Green : Colors.Red;
+        }
+
+        //Color
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return ((Color)value).Equals(Colors.Green);
+        }
+    }
+}
---
+++ ToDoList/Converter/BoolToImageSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ToDoList.Converter
+{
+    public class BoolToImageSource : IValueConverter
+    {
+        string iconDone = "icon_done50.png";
+        string iconNotDone = "icon_not_done50.png";
+
+        //Bool
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return (bool)value ? iconDone : iconNotDone;
+        }
+
+        //Color
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return ((FileImageSource)value).File == iconDone;
+        }
+    }
+}
